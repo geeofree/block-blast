bffaadb7e83b34e9515b163e7e584de436e1fefb
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2024-08-01 20:29:47 +0800

    update: provide container to center board

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
+import styled from "styled-components";
 import { Tiles } from "./components";
+
+const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export function App() {
   return (
-    <Tiles
-      width={512}
-      height={512}
-      board={[
-        [{}, {}, {}, {}, {}, {}, {}, {}],
-        [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
-        [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
-        [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
-        [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
-        [{}, {}, { color: "blue" }, { color: "blue" }, {}, {}, {}, {}],
-        [{}, {}, {}, { color: "blue" }, { color: "blue" }, {}, {}, {}],
-        [{}, {}, {}, {}, {}, {}, {}, {}],
-      ]}
-    />
+    <AppContainer>
+      <Tiles
+        width={512}
+        height={512}
+        board={[
+          [{}, {}, {}, {}, {}, {}, {}, {}],
+          [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
+          [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
+          [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
+          [{}, {}, {}, {}, {}, {}, {}, { color: "red" }],
+          [{}, {}, { color: "blue" }, { color: "blue" }, {}, {}, {}, {}],
+          [{}, {}, {}, { color: "blue" }, { color: "blue" }, {}, {}, {}],
+          [{}, {}, {}, {}, {}, {}, {}, {}],
+        ]}
+      />
+    </AppContainer>
   );
 }
