6fa77f1bf0b516ffd4f5fb745dcf2c8cb8b1f808
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2025-08-24 07:22:02 +0800

    fix: make rows and columns positioning properly work for Flex entity

--- src/entities/Flex.ts
+++ src/entities/Flex.ts
@@ -13,15 +13,43 @@ export class Flex extends Entity {
   }
 
   render(): void {
-    let prevX = 0;
-    let prevY = 0;
+    let largestY = 0;
+    let x = 0;
+    let y = 0;
+    let prevRow = 0;
 
-    this.entities.forEach((entity, index) => {
-      const { width, height } = entity.getSize();
-      entity.setPosition(prevX, prevY);
-      this.attachChild(entity.container);
-      prevX = prevX + width + this.gap;
-      prevY = prevY + this.gap + (height * Math.floor(index / this.maxItemsPerRow))
+    this.entities
+      .map((entity, index) => {
+        const { height } = entity.getSize();
+        const row = Math.floor(index / this.maxItemsPerRow);
+        const col = index % this.maxItemsPerRow;
+
+        largestY = Math.max(largestY, height);
+
+        if (row != prevRow) {
+          y = this.gap + largestY
+
+          if (col === 0) {
+            largestY = y + largestY;
+          }
+
+          prevRow = row;
+        }
+
+        return { entity, y }
+      })
+      .forEach((item, index) => {
+        const { entity, y } = item;
+        const { width } = entity.getSize();
+        const col = index % this.maxItemsPerRow;
+
+        if (col === 0) {
+          x = 0;
+        }
+
+        entity.setPosition(x, y);
+        x = x + this.gap + width;
+        this.attachChild(entity.container);
     });
   }
 }
